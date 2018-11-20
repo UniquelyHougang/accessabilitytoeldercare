3bfcb9ed4bfc14dd20ae9310f2eb902d617a053f
UniquelyHougang <45200553+UniquelyHougang@users.noreply.github.com>
2018-11-20 20:33:44 +0800

    Add files via upload

---
+++ docs/data/Eldercare_2.js
@@ -0,0 +1,10 @@
+var json_Eldercare_2 = {
+"type": "FeatureCollection",
+"name": "Eldercare_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": 24.0, "ADDRESSBLO": null, "ADDRESSBUI": null, "ADDRESSPOS": "530025", "ADDRESSSTR": "Blk 25 Hougang Avenue 3 #01-442", "ADDRESSTYP": null, "DESCRIPTIO": null, "HYPERLINK": null, "LANDXADDRE": 0.0, "LANDYADDRE": 0.0, "NAME": "Jia Ying Senior Activity Centre @ 25 (Branch)", "PHOTOURL": null, "ADDRESSFLO": null, "INC_CRC": "8D0E99AE1FBD4D75", "FMEL_UPD_D": "2016-07-28", "ADDRESSUNI": null, "X_ADDR": 34497.8222, "Y_ADDR": 38651.7401, "POI_CODE": 1.0 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 103.891705976408474, 1.365826415665798 ] ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 64.0, "ADDRESSBLO": null, "ADDRESSBUI": null, "ADDRESSPOS": "533174", "ADDRESSSTR": "Blk 174C Hougang Ave 1", "ADDRESSTYP": null, "DESCRIPTIO": null, "HYPERLINK": null, "LANDXADDRE": 0.0, "LANDYADDRE": 0.0, "NAME": "Fei Yue Senior Group Home @ Hougang", "PHOTOURL": null, "ADDRESSFLO": null, "INC_CRC": "53FB61A0BB730ED2", "FMEL_UPD_D": "2016-07-28", "ADDRESSUNI": null, "X_ADDR": 34207.1075, "Y_ADDR": 36804.6616, "POI_CODE": 1.0 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 103.889093324154445, 1.349122166015118 ] ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 116.0, "ADDRESSBLO": null, "ADDRESSBUI": null, "ADDRESSPOS": "530019", "ADDRESSSTR": "Blk 19 Hougang Avenue 3 #01-187", "ADDRESSTYP": null, "DESCRIPTIO": null, "HYPERLINK": null, "LANDXADDRE": 0.0, "LANDYADDRE": 0.0, "NAME": "Jia Ying Senior Activity Centre", "PHOTOURL": null, "ADDRESSFLO": null, "INC_CRC": "331932E8EBF0B639", "FMEL_UPD_D": "2016-07-28", "ADDRESSUNI": null, "X_ADDR": 34509.7681, "Y_ADDR": 38426.5208, "POI_CODE": 1.0 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 103.891813270191705, 1.363789611105266 ] ] } }
+]
+}
